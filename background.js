08eff0e432ec991c04e0bb776c1b32d9fd374cae
Tiago <tiagolpadua@gmail.com>
2022-08-17 17:46:15 -0300

    v1.0.0

--- background.js
+++ background.js
@@ -1,8 +1,21 @@
-// background.js
-
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        "title": 'Iniciar conversa no WhatsApp com "%s"',
+        "contexts": ["selection"],
+        "id": "menu-wz-opener"
+    });
 });
+    
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    let phoneNumber = info.selectionText.replace(/\D/g,'');
+
+    if (phoneNumber.indexOf("55") !== 0) {
+        phoneNumber = "55" + phoneNumber;
+    }
+
+    if (phoneNumber.length > 0) {
+        chrome.tabs.create({  
+            url: "https://api.whatsapp.com/send?phone=55" + phoneNumber
+        });
+    }
+})
